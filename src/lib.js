07b6b40721c1d37eb309baadfbed176b469672c7
chris48s <chris48s@users.noreply.github.com>
2026-02-07 13:20:38 +0000

    update actions/core import

--- src/lib.js
+++ src/lib.js
@@ -1,5 +1,5 @@
 import { promises as fs } from "fs";
-import core from "@actions/core";
+import * as core from "@actions/core";
 import { XMLParser, XMLValidator } from "fast-xml-parser";
 import { BaseAction } from "@action-badges/core";
 
